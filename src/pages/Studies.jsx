2c7a029209eb1cd3b737236c4186e60c6ba00620
Dayloc Yumar <daylocyumar@gmail.com>
2025-07-18 12:04:00 +0200

    error de rutas

--- src/pages/Studies.jsx
+++ src/pages/Studies.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import titulo from "src/assets/img/Titulo.jpg";
+import titulo from "../assets/img/Titulo.jpg";
 import { useNavigate } from "react-router-dom"; 
 
 function Studies() {
