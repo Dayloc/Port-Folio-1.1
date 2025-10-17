815beec13344353c31ce6515261de7f70ad1c6f5
Dayloc <158208881+Dayloc@users.noreply.github.com>
2025-10-17 14:43:48 +0000

    Updates

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		
-		<p className="text-warning">
-			Made with <i className="fa fa-heart text-danger" /> by  {" Dayloc "}
-			
-		</p>
-	</footer>
-);
