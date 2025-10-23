82fcbba7e743e9757bb1ef7c2bfd0ade963e251d
Dayloc <158208881+Dayloc@users.noreply.github.com>
2025-10-23 12:09:55 +0000

    CAmbiando estilos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
 
 	return (
 		<div className="container text-center mt-3 text-info ">
-		<h1 className=" text-warning mb-2">¡Hola! Soy Dayloc — Desarrollador apasionado y creador <br /> de experiencias web funcionales.</h1> 
+		<h1 className=" text-Info mb-2">¡Hola! Soy Dayloc — Desarrollador apasionado y creador <br /> de experiencias web funcionales.</h1> 
     	
 		
 		<p className="present  m-5">
@@ -17,9 +17,7 @@ export const Home = () => {
 
 	Te invito a que me conozcas y exploremos juntos mi trayectoria digital <br />así como  el futuro que podemos construir juntos!
     </p>
-	<div class="cuadro-fade">
-		<img src={follow_mw2} alt="" className="follow" onClick={()=>navigate('/myLife')}/>
-	</div>
+	
 	<div>
 		<button className="btn btn-danger"  onClick={()=>navigate('/myLife')} > Next</button>
 	</div>
